a73b3a1f2a6c7e537d9723db97c87b5cc5cde7dd
hjmh <hjmhaberkern@gmail.com>
2021-02-05 12:54:00 -0500

    added convenience functions to vizualize rois

--- fly2p/viz/viz.py
+++ fly2p/viz/viz.py
@@ -8,3 +8,24 @@ def myAxisTheme(myax):
     myax.get_yaxis().tick_left()
     myax.spines['top'].set_visible(False)
     myax.spines['right'].set_visible(False)
+    
+    
+    
+## ROI vizualization
+
+# plot shapely objects over reference image
+def plotShapelyLine(ax, shapelyLine, labelStr, col):
+    ax.plot(shapelyLine.coords.xy[0], shapelyLine.coords.xy[1], color=col)
+    ax.text(shapelyLine.coords[0][0], shapelyLine.coords[0][1]+2, labelStr, color=col, fontsize=12)
+    return ax
+
+def plotShapelyPoly(ax, shapelyPoly, labelStr,col):
+    ax.plot(shapelyPoly.coords.xy[0],EBroiRing.coords.xy[1], color=col)
+    ax.text(shapelyPoly.coords[0][0], EBroiRing.coords[0][1], labelStr, color=col, fontsize=12)
+    ax.plot(shapelyPoly.centroid.coords.xy[0],EBroiRing.centroid.coords.xy[1],'o')
+    return ax
+
+def plotEllipse(ax, ctr, longax, shortax, ellipseRot,col, alphaval):
+    ellipsepatch = ppatch.Ellipse(ctr, longax, shortax, -ellipseRot, alpha=alphaval, color=col)
+    ax.add_patch(ellipsepatch)
+    return ax
