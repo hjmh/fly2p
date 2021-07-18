31074f542254b5347fd57d48de9a89887a43ffc6
hjmh <hjmhaberkern@gmail.com>
2021-07-18 15:55:05 -0400

    added DFF plotting function

--- fly2p/viz/viz.py
+++ fly2p/viz/viz.py
@@ -2,8 +2,8 @@ from matplotlib import pyplot as plt
 import numpy as np
 import matplotlib.patches as ppatch
 
-## General
-# axis beautification
+# General ......................................................................
+## axis beautification
 def myAxisTheme(myax):
     myax.get_xaxis().tick_bottom()
     myax.get_yaxis().tick_left()
@@ -13,7 +13,7 @@ def myAxisTheme(myax):
 def plotScaleBar(ax,xlen,pos,labeltext):
     ax.plot([pos[0],pos[0]+xlen],[pos[1],pos[1]],'k')
     ax.text(pos[0],pos[1],labeltext)
-    
+
 def minimalAxisTheme(myax, xlen,pos,labeltext):
     plotScaleBar(myax,xlen,pos,labeltext)
     myax.axis('off')
@@ -30,11 +30,21 @@ def pathPlotAxisTheme(myax, units):
     myax.set_xlabel('x [{}]'.format(units))
     myax.set_ylabel('y [{}]'.format(units))
 
-    
-    
-## ROI vizualization
+# Calcium traces vizualization .................................................
+def plotDFFheatmap(time, roidat, ax):
+    cax = ax.pcolor(time,np.arange(0,roidat.shape[0]+1),roidat,cmap='Blues', edgecolors='face',shading='auto')
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel('\nROIs (n = {0})'.format(roidat.shape[0]))
 
-# plot shapely objects over reference image
+    # Add colorbar, make sure to specify tick locations to match desired ticklabels
+    cbar = fig.colorbar(cax)
+    cbar.set_label('$(F - F_0) / F_0$ (per ROI)')  # vertically oriented colorbar
+
+    return ax
+
+# ROI vizualization ............................................................
+
+## plot shapely objects over reference image
 def plotShapelyLine(ax, shapelyLine, labelStr, col):
     ax.plot(shapelyLine.coords.xy[0], shapelyLine.coords.xy[1], color=col)
     ax.text(shapelyLine.coords[0][0], shapelyLine.coords[0][1]+2, labelStr, color=col, fontsize=12)
